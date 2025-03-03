c28c70cdaafba85af969a3f942fb4daf107a8d0e
Tracker215 <sam.park@octopusenergy.services>
2025-03-03 12:06:09 +0000

    Update and rename streamlit_app.py to Compliance Database.py

---
+++ Compliance Database.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Compliance Database")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.write("hello")
+
+.stApp {
+    background-color: #100030;
+    color: white;
+}
